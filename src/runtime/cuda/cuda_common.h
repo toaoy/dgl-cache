bb0adbeb9fa3dd2ec66f9932dcdf41ec0b8bf9d7
Muhammed Fatih Balin <m.f.balin@gmail.com>
2022-10-29 19:10:04 +0000

    refactor importance_sampling part out to a function

--- src/runtime/cuda/cuda_common.h
+++ src/runtime/cuda/cuda_common.h
@@ -38,7 +38,7 @@ class CUDAWorkspaceAllocator {
  public:
   typedef char value_type;
 
-  void operator()(void* ptr) {
+  void operator()(void* ptr) const {
     runtime::DeviceAPI::Get(ctx)->FreeWorkspace(ctx, ptr);
   }
 
@@ -47,16 +47,16 @@ class CUDAWorkspaceAllocator {
   CUDAWorkspaceAllocator & operator=(const CUDAWorkspaceAllocator &) = default;
 
   template <typename T>
-  std::unique_ptr<T, CUDAWorkspaceAllocator> alloc_unique(std::size_t size) {
+  std::unique_ptr<T, CUDAWorkspaceAllocator> alloc_unique(std::size_t size) const {
     return std::unique_ptr<T, CUDAWorkspaceAllocator>(reinterpret_cast<T *>(
         runtime::DeviceAPI::Get(ctx)->AllocWorkspace(ctx, sizeof(T) * size)), *this);
   }
 
-  char *allocate(std::ptrdiff_t size) {
+  char *allocate(std::ptrdiff_t size) const {
     return reinterpret_cast<char *>(runtime::DeviceAPI::Get(ctx)->AllocWorkspace(ctx, size));
   }
 
-  void deallocate(char* ptr, std::size_t) {
+  void deallocate(char* ptr, std::size_t) const {
     runtime::DeviceAPI::Get(ctx)->FreeWorkspace(ctx, ptr);
   }
 };

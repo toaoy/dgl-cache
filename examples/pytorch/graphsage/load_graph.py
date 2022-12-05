61aef86baa4dd9ef1ef67f56e4991b0d7330208f
Muhammed Fatih Balin <m.f.balin@gmail.com>
2022-12-05 20:02:42 +0000

    add mag240M

--- examples/pytorch/graphsage/load_graph.py
+++ examples/pytorch/graphsage/load_graph.py
@@ -13,40 +13,6 @@ def load_reddit(self_loop=True):
     g.ndata["labels"] = g.ndata.pop("label")
     return g, data.num_classes
 
-
-def load_ogb(name, root="dataset"):
-    from ogb.nodeproppred import DglNodePropPredDataset
-
-    print("load", name)
-    data = DglNodePropPredDataset(name=name, root=root)
-    print("finish loading", name)
-    splitted_idx = data.get_idx_split()
-    graph, labels = data[0]
-    labels = labels[:, 0]
-
-    graph.ndata["features"] = graph.ndata.pop("feat")
-    graph.ndata["labels"] = labels.long()
-    in_feats = graph.ndata["features"].shape[1]
-    num_labels = len(th.unique(labels[th.logical_not(th.isnan(labels))]))
-
-    # Find the node IDs in the training, validation, and test set.
-    train_nid, val_nid, test_nid = (
-        splitted_idx["train"],
-        splitted_idx["valid"],
-        splitted_idx["test"],
-    )
-    train_mask = th.zeros((graph.number_of_nodes(),), dtype=th.bool)
-    train_mask[train_nid] = True
-    val_mask = th.zeros((graph.number_of_nodes(),), dtype=th.bool)
-    val_mask[val_nid] = True
-    test_mask = th.zeros((graph.number_of_nodes(),), dtype=th.bool)
-    test_mask[test_nid] = True
-    graph.ndata["train_mask"] = train_mask
-    graph.ndata["val_mask"] = val_mask
-    graph.ndata["test_mask"] = test_mask
-    print("finish constructing", name)
-    return graph, num_labels
-
 def load_mag240m(root="dataset"):
     from ogb.lsc import MAG240MDataset
     import numpy as np
@@ -86,6 +52,42 @@ def load_mag240m(root="dataset"):
     g.ndata["labels"][train_nid] = labels[train_nid - paper_offset].long()
     g.ndata["labels"][val_nid] = labels[val_nid - paper_offset].long()
     return g, num_labels
+
+def load_ogb(name, root="dataset"):
+    if name == "ogbn-mag240M":
+        return load_mag240m(root)
+
+    from ogb.nodeproppred import DglNodePropPredDataset
+
+    print("load", name)
+    data = DglNodePropPredDataset(name=name, root=root)
+    print("finish loading", name)
+    splitted_idx = data.get_idx_split()
+    graph, labels = data[0]
+    labels = labels[:, 0]
+
+    graph.ndata["features"] = graph.ndata.pop("feat")
+    graph.ndata["labels"] = labels.long()
+    in_feats = graph.ndata["features"].shape[1]
+    num_labels = len(th.unique(labels[th.logical_not(th.isnan(labels))]))
+
+    # Find the node IDs in the training, validation, and test set.
+    train_nid, val_nid, test_nid = (
+        splitted_idx["train"],
+        splitted_idx["valid"],
+        splitted_idx["test"],
+    )
+    train_mask = th.zeros((graph.number_of_nodes(),), dtype=th.bool)
+    train_mask[train_nid] = True
+    val_mask = th.zeros((graph.number_of_nodes(),), dtype=th.bool)
+    val_mask[val_nid] = True
+    test_mask = th.zeros((graph.number_of_nodes(),), dtype=th.bool)
+    test_mask[test_nid] = True
+    graph.ndata["train_mask"] = train_mask
+    graph.ndata["val_mask"] = val_mask
+    graph.ndata["test_mask"] = test_mask
+    print("finish constructing", name)
+    return graph, num_labels
 
 def inductive_split(g):
     """Split the graph into training graph, validation graph, and test graph by training
